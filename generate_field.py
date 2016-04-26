610bd43ef9d71d3c13db91dc7850dafc86731f73
l7opy4uk <smoke_sev@list.ru>
2016-04-26 14:07:05 +0300

    Actual commit

--- generate_field.py
+++ generate_field.py
@@ -1,40 +1,25 @@
 '''
 What it do:
 1. Generates the field. So far it filled up with symbols "0".
-2. change_field function defined, by changing it you can reassign values that will be printed in final.
+2. change_field function defined, by changing it you can reassign values that
+will be printed in final.
 (it will be useful for taking input from user, or view visual changes)
 3. Generate a list, to numerate a columns in alphabetical.
 4. Generate a list, to numerate a rows in digital.
 
+
 Tips for future:
-- can to add set_defualts function to put there symbols that will fill field, maybe var with dimension of field'''
+- can to add set_defaults function to put there symbols that will fill field,
+maybe var with dimension of field
+'''
+FIELD_WIDTH = 10
+FIELD_HEIGHT = 10
 
 
-def generate_field(x=10, y=10):
+def generate_field(x=FIELD_WIDTH, y=FIELD_HEIGHT):
     '''Generate field
-    at the moment it have const parameters,
-    x = 10 (elements in nested list)
-    y = 10 (element in list)
-    but it could be easily changed just by typing var in function'''
-    field = [[0 for i in range(x)] for j in range(y)]
-    return field
+    '''
+    return [[0 for _i in range(x)] for _j in range(y)]
 
 
-def change_field(l):
-    '''Function for making changes in field.
-    It's created for future development. For now it's doing nothing.
-    It just take the list and return it back without changes'''
-    return l
-
-
-def create_list_alphabet():
-    '''Creates a list of letter in alphabetical.'''
-    l = [chr(i) for i in range(65, 90)]
-    return l
-
-
-def generate_dig_list(x = 10):
-    '''Generate list'''
-    l = [i for i in range(1, x + 1)]
-    return l
 
---
+++ install_dot.py
@@ -0,0 +1,129 @@
+'''
+TODO value check in install_dot
+TODO same_dots list should to be relocated. to refresh correctly
+'''
+import string
+from generate_field import generate_field
+from display_field import output_field
+
+field = generate_field()
+
+
+def make_dict():
+    '''
+    Create a dictionary with numerate letters.
+    Using for translation letters into digits
+    :return:
+    '''
+    key = list(string.ascii_uppercase)
+    value = [i for i in range(0, 25)]
+    dict = {}
+    for i, j in zip(key, value):
+        dict[i] = j
+    return dict
+
+
+def check_position(x, y):
+    l = field
+    if l[y - 1][dict[x]] != 0:
+        print("Position already occupied")
+
+
+def check_around(y, x):
+    '''
+    Check position for similar marker in position around,
+    if marker the same = append position to the same_dots list
+    :param x:
+    :param y:
+    :return:
+    '''
+    list_yx = [[-1, 1], [-2, 1], [-2, 0], [-2, -1], [-1, -1], [0, -1], [0, 0], [0, 1]]
+    marker = 1
+    same_dots = []
+    for i in list_yx:
+        if field[y + i[0]][x + i[1]] == marker:
+            same_dots.append([y + i[0], x + i[1]])
+    return same_dots
+
+def create_path(y, x):
+    dot_alpha = [y - 1, x]
+    print(dot_alpha)
+    dot_start = dot_alpha
+    dot_next = []
+    dots_around = check_around(y, x)
+    print(dots_around)
+    dots_path = []
+    if dots_around != []:
+        print("work bitch!")
+        while dot_alpha != dot_next:
+            for i in dots_around:
+                dot_next = [i[0]+1, i[1]]
+                print(dot_next)
+                dots_around = check_around(i[0]+1, i[1])
+                if dot_next != dot_start:
+                    dot_start = dot_next
+                    dots_path.append(dot_start)
+                    #if dots_around == []:
+                break
+
+                  #  print(dots_around)
+                  #  print(dots_path)
+                  #  for i in dots_around:
+                  #      dots_around = check_around(i[1], i[0])
+                  #      dot_next = [i[0], i[1]]
+                  #      print("part 2")
+                  #      if dot_next != dot_start:
+                  #          print("part 3")
+                  #          dot_start = dot_next
+                  #          dots_path.append(dot_start)
+                  #          if dots_around == []:
+                  #              break
+
+                  #          print(dots_around)
+                  #          print(dots_path)
+        else:
+            print(dots_path)
+
+
+
+def install_dot(marker):
+    '''
+    Check if position is free, if True:
+        install dot in choosen position.
+    :param marker: set a marker param you want to display
+    :return:
+    '''
+    l = field
+    dict = make_dict()
+    while True:
+        x, y = take_input()
+        x = dict[x]
+        if l[y - 1][x] != 0:
+            print("Position is already occupied")
+            continue
+        else:
+            l[y - 1][x] = marker
+        create_path(y, x)
+        return l
+
+
+def take_input():
+    '''
+    Takes x and y params from user
+    :return:
+    '''
+    input_y = int(input("y?"))
+    input_x = input("x?").upper()
+    return input_x, input_y
+
+
+field[2][1] = 1
+field[3][1] = 1
+field[1][2] = 1
+print(output_field(field))
+while True:
+    output_field(install_dot(1))
+
+
+
+
